1761088f123e3062dbb5b89433b097f905b4d66c
NazarLukashik <nazarlykashik@gmail.com>
2022-07-08 00:05:53 +0300

    logic work!

--- Login page/ViewController.swift
+++ Login page/ViewController.swift
@@ -7,28 +7,37 @@
 
 import UIKit
 
-let users: [String] = ["User"]
-let paswords: [String] = ["Pasword"]
+
 
 class ViewController: UIViewController {
     @IBOutlet weak var userName: UITextField!
-    @IBOutlet weak var pasword: UITextField!
+    @IBOutlet weak var password: UITextField!
     @IBOutlet weak var login: UIButton!
     @IBOutlet weak var loginTXT: UIButton!
     @IBOutlet weak var paswordTXT: UIButton!
+    let storedUsername = "User"
+    let storedPassword = "Pasword"
+    var authenticationDidFail: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     @IBAction func checkLogin(_ sender: Any) {
-        for (first, first) in zip(users.first, users.first) {
-            if userName.text == users.username && pasword.text == users.pasword{
+        if self.userName.text == storedUsername && self.password.text == storedPassword {
+            self.authenticationDidFail = true
+            print("nkjdfn")
+            let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            let WelcomeViewController = storyBoard.instantiateViewController(withIdentifier: "vc") as! WelcomeViewController
+            self.present(WelcomeViewController, animated: true, completion: nil)
                 
             }
         }
-    }
+
     
 
-}
 
+
+
+    
+}
