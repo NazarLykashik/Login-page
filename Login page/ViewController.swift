4ec1dd97097a2d7661917e7181f25d3c098bbc66
NazarLukashik <nazarlykashik@gmail.com>
2022-07-12 11:29:05 +0300

    added alerts with info about login and password

--- Login page/ViewController.swift
+++ Login page/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var loginTXT: UIButton!
     @IBOutlet weak var paswordTXT: UIButton!
     let storedUsername = "User"
-    let storedPassword = "Pasword"
+    let storedPassword = "Password"
     var authenticationDidFail: Bool = false
     
     override func viewDidLoad() {
@@ -29,12 +29,24 @@ class ViewController: UIViewController {
             let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
             let WelcomeViewController = storyBoard.instantiateViewController(withIdentifier: "vc") as! WelcomeViewController
             self.present(WelcomeViewController, animated: true, completion: nil)
-                print("fljsd")
             }
         }
 
+    @IBAction func forgotLogin(_ sender: Any) {
+        let loginAlert = UIAlertController(title: "Login", message: "Your login is: User", preferredStyle: .alert)
+        let loginAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        loginAlert.addAction(loginAction)
+        present(loginAlert, animated: true, completion: nil)
+    }
     
-
+    @IBAction func forgotPassword(_ sender: Any) {
+        let passwordAlert = UIAlertController(title: "Password", message: "Your password is: Password", preferredStyle: .alert)
+        let passwordAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        passwordAlert.addAction(passwordAction)
+        present(passwordAlert, animated: true, completion: nil)
+        
+    }
+    
 
 
 
